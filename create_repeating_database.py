41fb696b854e2d14c6e3f1d63950df62d310ef02
Luis Antonio Dominguez <ladominguez@ucla.edu>
2022-10-14 11:55:17 -0500

    unixtime added to the database.

--- create_repeating_database.py
+++ create_repeating_database.py
@@ -1,11 +1,13 @@
 import sqlite3
 import os
 import json
+import time
+import pytz
+from datetime import datetime
 from tqdm import tqdm
 
 config_file = open('config.json')
-config = json.load(config_file) 
-
+config = json.load(config_file)
 
 
 def create_table(filename):
@@ -19,6 +21,7 @@ def create_table(filename):
     create_table = """ CREATE TABLE catalog(
         ID          INTEGER PRIMARY KEY     AUTOINCREMENT,
         datetime    CHAR(22)                NOT NULL,
+		unixtime    FLOAT                   NOT NULL,
 		latitude    FLOAT                   NOT NULL,
 		longitude   FLOAT                   NOT NULL,
 		depth       FLOAT                   NOT NULL,
@@ -26,31 +29,36 @@ def create_table(filename):
 		eq_id       INTEGER                 NOT NULL,
         tweet       BOOLEAN                 NOT NULL,
         tweet_id    VARCHAR(30),
+		post_time   CHAR(22),
         repeater    BOOLEAN                 NOT NULL,
-        sequence_id INTEGER                  
+        sequence_id INTEGER,
+		tweet_text  TEXT
     )
     """
     con.execute(create_table)
     con.close()
 
+
 if not os.path.isfile(config['database']):
-	create_table(config['database'])	
+    create_table(config['database'])
 
 con = sqlite3.connect(config['database'])
 cursor = con.cursor()
-with open(config['catalog'],'r') as f:
+with open(config['catalog'], 'r') as f:
     for line in tqdm(f):
         entries = line.split()
-        entries[0:2] = ['T'.join(entries[0:2])] # merges date and time columns
-        entries.append(False) # tweet (False for catalog) 
+        entries[0:2] = ['T'.join(entries[0:2])]  # merges date and time columns
+        unixtime = datetime.strptime(
+            entries[0], "%Y/%m/%dT%H:%M:%S.%f").replace(tzinfo=pytz.UTC).timestamp()
+        entries.insert(1, unixtime)
+        entries.append(False)  # tweet (False for catalog)
         entries.append(None)  # tweet_id None for catalog
-        entries.append(False) # Set repeater to false at the beginning
+        entries.append(None)  # Post time, None for catalog
+        entries.append(False)  # Set repeater to false at the beginning
         entries.append(None)  # Set sequence_id to None
-        add_entry = '''INSERT INTO catalog(datetime, latitude, longitude, depth, mag, eq_id, tweet, tweet_id, repeater, sequence_id) VALUES (?,?,?,?,?,?,?,?,?,?);'''
-        cursor.execute(add_entry,entries)
+        entries.append('')    # Tweet empty for catalog
+        add_entry = '''INSERT INTO catalog(datetime, unixtime, latitude, longitude, depth, mag, eq_id, tweet, tweet_id, post_time, repeater, sequence_id, tweet_text) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?);'''
+        cursor.execute(add_entry, entries)
 
 con.commit()
 con.close()
-
-
-
