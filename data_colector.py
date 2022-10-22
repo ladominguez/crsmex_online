da229ce107a717d5f6acf62717aaebe484dd7dd4
Luis Antonio Dominguez <ladominguez@ucla.edu>
2022-10-21 20:59:37 -0500

    updating downnloaded_data done.

--- data_colector.py
+++ data_colector.py
@@ -1,9 +1,12 @@
 from importlib.metadata import entry_points
+from re import I
 from subprocess import Popen, PIPE, DEVNULL
 from tkinter import W
+from urllib.parse import _NetlocResultMixinBytes, _NetlocResultMixinStr
 import pandas as pd
 import sqlite3
 import os
+import glob
 import json
 from datetime import datetime, timedelta
 from geopy.distance import great_circle
@@ -59,8 +62,7 @@ def stp_generator():
         cursor.execute(update_nearby_sta, (','.join(nearby_stations), entry['tweet_id']))
         con.commit()
     con.close
-        # try:
-        #    fid.close()
+    return None
 
 def data_colector():
     con = sqlite3.connect(os.path.join(root_crsmex, config['database']))
@@ -76,18 +78,39 @@ def data_colector():
                p = Popen(config["SSNstp"],stdin=PIPE, stdout=DEVNULL, stderr=DEVNULL, bufsize=0) 
                p.communicate(input.encode('ascii'))
                os.system('ls')
+    return None
 
 def check_collected_data():
     con = sqlite3.connect(os.path.join(root_crsmex, config['database'])) 
     cursor = con.cursor()
     directories = os.listdir(os.path.join(root_crsmex,'tmp'))
-    cmd_sql = r"select nearby_sta from twitter where tweet_id= " + directories[0] + ";"
-    cursor.execute(cmd_sql)
 
-    results = cursor.fetchall()
-    print(cmd_sql)
-    for x in results:
-        print(x)
+    for directory in directories:
+        cmd_sql = r"select nearby_sta, tweet_id from twitter where tweet_id= " + directory + ";"
+        cursor.execute(cmd_sql)
+        results = cursor.fetchall()
+        if len(results) >= 2:
+            raise NameError('Duplicated records with the same tweet id.')
+        
+        stations, tweet_id = results[0]
+        Nsta=len(stations.split(','))
+        print('Nsta: ', Nsta, ' tweet_id: ', tweet_id)
+        files_found = glob.glob(os.path.join(root_crsmex,'tmp',tweet_id,'*.sac'))
+        
+        if len(files_found) == Nsta*3:
+            print('UPDATING: ', tweet_id)
+            update_downloaded_data = '''UPDATE twitter
+                                   SET data_downloaded = ?
+                                   WHERE tweet_id = ?'''
+            cursor.execute(update_downloaded_data, (True, tweet_id))
+            con.commit()
+    con.close()
+
+    return None
+        
+
+
+
 
 
 
