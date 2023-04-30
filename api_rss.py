1e46b2d86e7b9ba481bb9b921ecb8e901ff7fc6c
Luis A. Dominguez <ladominguez@ucla.edu>
2023-04-29 22:16:43 -0600

    Working on rss feeder

--- api_rss.py
+++ api_rss.py
@@ -1,4 +1,5 @@
 from data_colector import *
+import time as tt
 import feedparser as fp
 from html.parser import HTMLParser
 
@@ -33,3 +34,25 @@ log.addHandler(fh)
 
 if __name__ == '__main__':
     while True:
+        print("====================="+tt.strftime("%Y/%M/%d %H:%M:%S", tt.localtime()) + "===========================")
+        rss=fp.parse(config['rss_feed'])
+        start_tags = [] 
+        end_tags = []
+        all_data = []
+        comments = []
+        parser = Parser()
+        for entry in rss.entries:
+            Html_Data = entry['summary_detail']['value']
+            parser.feed(Html_Data)
+            date = all_data[0].split()[0].split(':')[1]
+            time = all_data[0].split()[1]
+            latitude = all_data[1].split()[1].split('/')[0]
+            longitude = all_data[1].split()[1].split('/')[1]
+            depth = all_data[2].split()[1]
+            mag = entry['title'].split(',')[0]
+            print('date: ', date + ',' + time + ' lat: ' + str(latitude) + ' lon: ' + str(longitude) + ' depth: ' + depth + ' mag: ' + mag)
+        tt.sleep(30)
+
+
+
+
